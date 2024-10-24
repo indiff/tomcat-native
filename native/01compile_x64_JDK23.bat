32b8f8705018752e5e1fd913472a23e25bf38792
indiff <indiff@126.com>
2024-10-24 17:39:06 +0800

    支持编译最新版本

--- native/01compile_x64_JDK23.bat
+++ native/01compile_x64_JDK23.bat
@@ -2,15 +2,15 @@
 rem call  c:\cmsc\setenv.bat x64
 call "C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\Tools\VsDevCmd.bat" -arch=x64
 
-set JAVA_HOME=D:\java\jdk1.7.0_80
+set JAVA_HOME=C:\Users\Administrator\.jdks\azul-23
 set OPENSSL_VER=3.0.13
 set APR_VER=1.7.x
 set WINVER=WIN7
 
 rd -s -q WIN7_X64_DLL_RELEASE
 
-
 nmake -f NMAKEMakefile BUILD_CPU=x64 WITH_APR=C:\deps-x64\apr-%APR_VER% WITH_OPENSSL=C:\deps-x64\openssl-%OPENSSL_VER% APR_DECLARE_STATIC=1 OPENSSL_NEW_LIBS=0
 
-move WIN7_X64_DLL_RELEASE WIN7_X64_DLL_RELEASE_JDK7
+move WIN7_X64_DLL_RELEASE WIN7_X64_DLL_RELEASE_JDK23
+
 pause
